45c5fe2a742dc29cb3313870cad7fe8c531459f3
Daniel Klotz <ido87@Daniels-iMac.local>
2015-10-29 19:26:18 +0100

    Separated function into own files

--- App/calculations.R
+++ App/calculations.R
@@ -5,7 +5,9 @@ require("xts")
 require("dplyr")
 require("grid")
 require("gridExtra")
-oldw <- getOption("warn")
+# 
+source(paste(ctrl$pathtoApp,"/f_expanded_barplots.R",sep=""))
+source(paste(ctrl$pathtoApp,"/f_rasterplot_functions.R",sep=""))
 ######################################################################################
 # read in 
 ######################################################################################
@@ -27,14 +29,14 @@ tmp <- readLines( paste(  ctrl$ofoldername, "/Statistics.txt", sep="") )
 lngth_spinup <- grep("start time-step of evaluation",tmp) %>% tmp[.] %>% sub('.*:', '',.) %>% as.integer(.)
 lngth_sim <- dim(d_runoff)[1] 
 d_runoff <- slice(d_runoff,lngth_spinup:lngth_sim)
-#add a nice formated date to the variables
-rdate  <- paste(d_runoff$yyyy,
+# add a (nice) formated date to the variables
+rdate <- paste(d_runoff$yyyy,
                 sprintf("%02d",d_runoff$mm),
                 sprintf("%02d",d_runoff$dd),
                 sprintf("%02d",d_runoff$hh),
                 sprintf("%02d",d_runoff$mm.1),
                 sep= "" ) %>% 
-  as.POSIXct(format = "%Y%m%d%H%M",tz="UTC")
+          as.POSIXct(format = "%Y%m%d%H%M",tz="UTC")
 d_runoff$rdate <- rdate
 #
 lngth_sim <- dim(d_runoff)[1] 
@@ -94,155 +96,12 @@ pBIAS_total <- hydroGOF::pbias(tempSIM,tempOBS)
 cor_total <- cor(tempSIM,tempOBS) %>% diag(.)
 # some cleaning
 rm(tempOBS,tempSIM)
-# write out .txt files (still curde !! )
+# write out NSE .txt files
+pathtoOut <- paste(ctrl$pathtoApp,"/out/",sep="")
 write.table(cbind(d_nums,t(NSE_hydyearly), NSE_total), 
-            file = paste(substr(ctrl$pathtoApp, 1, nchar(ctrl$pathtoApp)-3), "NSE_Hydyear.csv", sep=""),
+            file = paste(pathtoOut,"NSE_Hydyear.csv", sep=""),
             row.names = FALSE, col.names = c("#",paste("HY",hydyears_in_d),"TOTAL"), quote = FALSE, sep = ";")
 #
-# define raster plot function for hyd-yearly plots:
-plt_yOF <- function(OF_hydyearly,hydyears_in_d,eval_size,plt_ctrl) {
-  require("reshape2")
-  of_y <- expand.grid(hydyears = hydyears_in_d, numberBasins = 1:eval_size) 
-  temp <- OF_hydyearly; 
-  temp[temp < plt_ctrl$lb_cut] <- plt_ctrl$lb_cut
-  temp <- melt(temp)[3]
-  of_y$OFvalue = temp$value
-  #
-  plt_out <- ggplot(of_y, aes(hydyears,numberBasins, fill = OFvalue),environmnet = environment()) + 
-    ggtitle(plt_ctrl$gtitle) + 
-    geom_raster(position = "identity") + 
-    ylab(plt_ctrl$ylab) + 
-    xlab(plt_ctrl$xlab) + 
-    scale_y_reverse(breaks = 1:eval_size, labels = d_nums) + 
-    scale_x_discrete( breaks = hydyears_in_d) +
-    scale_fill_gradient2(space = "Lab", 
-                         low = plt_ctrl$clr1 , mid= plt_ctrl$clr2 , high = plt_ctrl$clr3 ,
-                         midpoint = plt_ctrl$midpoint, 
-                         limits= plt_ctrl$limits ,
-                         na.value = plt_ctrl$clr4) +
-    theme_bw(base_size = 15) +
-    theme( legend.position="none" )  + 
-    geom_tile(color = "white", size = 0.25 ) + 
-    geom_text(aes(hydyears,numberBasins, label = round(OFvalue,2)), size = ctrl$OFsize , color= "black")
-  return(plt_out)
-}
-
-# define rasterplot functions for total OF 
-plt_tOF <- function(OF_total,eval_size,plt_ctrl) {
-  of_t <- expand.grid(total = 1, numberBasins = 1:eval_size) 
-  temp <- OF_total; 
-  # replace values under lower boundary:
-  temp[temp < plt_ctrl$lb_cut] <- plt_ctrl$lb_cut 
-  # prepare dataframe for ggplot
-  temp <- melt(temp)
-  of_t$OFvalue = temp$value
-  #
-  plt_t <- ggplot(of_t , aes(total,numberBasins, fill = OFvalue),environmnet = environment()) +
-    geom_raster(position = "identity") +
-    ggtitle(plt_ctrl$gtitle) + 
-    theme_bw(base_size = 15) +
-    theme(axis.title.y = element_blank(), 
-          axis.title.x = element_blank(),
-          axis.text.y = element_blank(),
-          axis.text.x = element_blank(),
-          legend.text = element_text(size = 12),
-          axis.ticks = element_blank(),
-          panel.grid.major = element_blank(), 
-          panel.grid.minor = element_blank(), 
-          plot.margin = grid::unit(c(0.5,2,0.8,-0.5), "cm") ) +
-    geom_tile(color="white", size = 0.25) + 
-    geom_text(aes( total, numberBasins ,label = round(OFvalue,2) ), size = ctrl$OFsize ,color="black") +
-    scale_y_reverse() +
-    scale_fill_gradient2(space = "Lab",
-                         name = plt_ctrl$gtitle,
-                         low = plt_ctrl$clr1,
-                         mid= plt_ctrl$clr2, 
-                         high = plt_ctrl$clr3,
-                         midpoint = plt_ctrl$midpoint, 
-                         limits = plt_ctrl$limits,
-                         na.value = plt_ctrl$clr3)
-  return(plt_t)
-}
-
-list_yOF_barplts <- function(OF_hydyearly,eval_size,d_nums,d_OFyearly,plt_ctrl) {
-  # creates a list of bar-plots for a the yearly objective functions (OF)
-  # see: description-follows-soonTM
-  # ***
-  # calculations:
-  # replace values under lower boundary:
-  temp <- OF_hydyearly;
-  temp[temp < plt_ctrl$lb_cut] <- plt_ctrl$lb_cut 
-  # prepare data for plotting
-  d_OFyearly <- as.data.frame(temp)
-  newNames <- paste(plt_ctrl$gtitle,d_nums,sep="") 
-  names(d_OFyearly)  <- newNames
-  years_in_data_shrt <- as.character(years_in_data) %>% substring(.,3,4)
-  d_OFyearly$hydyear <- hydyears_in_d
-  # make list of plots
-  barplts <- list()
-  barplts <- lapply(1:eval_size, 
-                        function(k) ggplot(data = d_OFyearly,environmnet = environment()) +
-                                      geom_bar(stat="identity",
-                                               position = "identity",
-                                               aes_string(x="hydyear", y=newNames[k],fill = newNames[k])) +
-                                      theme_bw(base_size = 15) +
-                                      ggtitle(newNames[k]) +
-                                      xlab(plt_ctrl$xlab) +
-                                      ylab(plt_ctrl$ylab ) +
-                                      scale_y_continuous(limits = plt_ctrl$limits ) + 
-                                      theme(legend.position= "none", 
-                                            axis.text.x = element_text(angle = 50, hjust = 1), 
-                                            plot.margin = grid::unit(c(0.2,0.5,0.2,0.5), "cm") ) +
-                                      scale_fill_gradient2(space = "Lab", 
-                                                           low = plt_ctrl$clr1,
-                                                           mid = plt_ctrl$clr2, 
-                                                           high = plt_ctrl$clr3, 
-                                                           midpoint = plt_ctrl$midpoint, 
-                                                           limits = plt_ctrl$limits )
-            )
-  return(barplts)
-} 
-
-
-
-save_expnd_barplts <-function(list_barplts,eval_size,s_ctrl) {
-  # some pre sets for calculations:
-  num_plots <- ceiling(eval_size/9)
-  g <- seq(from = 1, to = (eval_size-9) , by=9)
-  max_plots <- (length(g)+1)
-  # connect to a html file in www folder 
-  fileConn <- file(paste(ctrl$pathtoApp,"/www/",s_ctrl$hmtlfilename,".html",sep=""),"w")
-    # write html header
-    writeLines(text = '<!DOCTYPE html>',fileConn)
-    writeLines(text = '<html>',fileConn)
-    writeLines(text = '<body>',fileConn)
-    # save everything localy & link it within the html file
-    for ( i in 1:(max_plots-1) )
-    {
-      j = g[i]
-      plt_name <- paste(s_ctrl$jpgfilename ,i,".jpg", sep = "")
-      plt_pathANDname <- paste(ctrl$pathtoApp,"/www/",plt_name,sep = "")
-      plt_hmtlInfos <- paste("<img src=\"",plt_name,'" alt="nothing" style="width:800px;height:500px;">' ,sep = "")
-      writeLines(text = plt_hmtlInfos,fileConn )
-      #
-      jpeg(file = plt_pathANDname, width = 800, height = 500, units = "px")
-      do.call("grid.arrange",c(list_barplts[j:(j+8)],list(ncol = 3, nrow = 3) ))
-      dev.off()
-      
-    }
-    plt_name <- paste(s_ctrl$jpgfilename ,i+1,".jpg", sep="")
-    plt_pathANDname <- paste(ctrl$pathtoApp,"/www/",plt_name,sep="")
-    plt_hmtlInfos <- paste("<img src=\"",plt_name,'" alt="nothing" style="width:800px;height:500px;">' ,sep = "")
-    #
-    writeLines( plt_hmtlInfos,fileConn)
-    jpeg(file = plt_pathANDname, width = 800, height = 500, units = "px")
-    do.call("grid.arrange",c(list_barplts[(j+9):eval_size],list(ncol = 3, nrow = 3) )) 
-    dev.off()
-  close(fileConn)
-}
-
-
-
 
 ######################################################################################
 # plots: NSE
