e5be13606ecc76ff3761c404d9615dd18d52f927
Daniel Klotz <ido87@Daniels-iMac.local>
2015-12-08 18:07:25 +0100

    start with the update
    a) got a rough idea about the possibilities and what can be done.
    a1) Best direction is probably to create R scripts for the file read in
    and something like an action button to execute them
    b) Rearranged layout

--- App/ui.R
+++ App/ui.R
@@ -1,36 +1,61 @@
-require(shiny)
+library(shiny)
+library(shinyFiles)
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  theme = "bootstrap.css",
-  # Application title
-  titlePanel(img(src="icon_cosvis.png", height = 210, width = 210)), 
-  h6("v0.2 by Klotz, Wesemann & Herrnegger"),
-  # runoff 
-  h3("<runoff check>"),
-  selectInput("basin_num",
-              "# basins:",
-              choices = d_nums, 
-              selected = 1, 
-              width = "100px"),
-  dygraphOutput("dygrph1", width = "100%", height = "400px"),
-  dygraphOutput("dygrph2", width = "100%", height = "100px"),
-  # of all in one:
-  h3("<summary tables>"),
-  tabsetPanel(tabPanel("-"),
-              tabPanel("NSE",plotOutput("NSE", width = "1050px", height = "1500px")),
-              tabPanel("KGE",plotOutput("KGE", width = "1050px", height = "1500px")),
-              tabPanel("pBIAS",plotOutput("pbias", width = "1050px", height = "1500px")),
-              tabPanel("CORR",plotOutput("corr", width = "1050px", height = "1500px")),
-              type = "pills"
-              ),
-  # splitted graphs:
-  h3("<separated graphs>"),
-  tabsetPanel(tabPanel("-"),
-              tabPanel("NSE",includeHTML("www/expnd_nse.html")),
-              tabPanel("KGE",includeHTML("www/expnd_kge.html")),
-              tabPanel("pBIAS",includeHTML("www/expnd_pbias.html")),
-              tabPanel("CORR",includeHTML("www/expnd_cor.html")),
-              type=  "pills"
-              )
-))
+# 
+shinyUI(
+  fluidPage(
+    theme = "bootstrap.css",
+    # header
+    h1(img(src="icon_cosvis.png", height = 210, width = 210), align="center"), 
+    h6("v0.2 by Klotz, Wesemann & Herrnegger", align="center"),
+    tags$hr(),  
+    sidebarPanel(
+      tags$p('Choose the App folder:'),
+      shinyDirButton('APPfolder', 
+                     label='App Folder', 
+                     title='Please select the App folder'
+      ),
+      tags$p(),
+      tags$p('Choose the COSEROreg folder:'),
+      shinyDirButton('COSfolder', 
+                      label='COSEROreg folder', 
+                      title='Please select the COSEROreg folder'
+                    ),
+      tags$p(),
+      actionButton("goButton", "Go!")
+    ),
+    mainPanel(
+  navbarPage("navigation!",
+    tabPanel("runoff",
+      # runoff 
+      h3("<runoff check>"),
+      selectInput("basin_num",
+                    "# basins:",
+                    choices = d_nums, 
+                    selected = 1, 
+                    width = "100px"),
+      dygraphOutput("dygrph1", width = "100%", height = "400px"),
+      dygraphOutput("dygrph2", width = "100%", height = "100px")
+    ),
+    tabPanel("stuff",
+      # of all in one:
+      h3("<summary tables>"),
+      tabsetPanel(tabPanel("-"),
+                  tabPanel("NSE",plotOutput("NSE", width = "1050px", height = "1500px")),
+                  tabPanel("KGE",plotOutput("KGE", width = "1050px", height = "1500px")),
+                  tabPanel("pBIAS",plotOutput("pbias", width = "1050px", height = "1500px")),
+                  tabPanel("CORR",plotOutput("corr", width = "1050px", height = "1500px")),
+                  type = "pills"
+                  ),
+      # splitted graphs:
+      h3("<separated graphs>"),
+      tabsetPanel(tabPanel("-"),
+                  tabPanel("NSE",includeHTML("www/expnd_nse.html")),
+                  tabPanel("KGE",includeHTML("www/expnd_kge.html")),
+                  tabPanel("pBIAS",includeHTML("www/expnd_pbias.html")),
+                  tabPanel("CORR",includeHTML("www/expnd_cor.html")),
+                  type=  "pills"
+                  )
+    )
+  ))))
--- start.R
+++ start.R
@@ -8,16 +8,14 @@
 ######################################################################################
  rm(list=ls())  # removes all the variables in the enviorments 
  ctrl <- list()  # variable pre-set for the conntrols
+ require(magrittr)
 #************************************************************************************
 # Paths:
 #  ctrl$pathtoCosero <- "C:/Users/H0740147/Cosero_Mur/COSERO/MitExcel" 
-#  ctrl$pathtoApp <- "C:/Users/H0740147/Cosero_Mur/COSERO/Auswertung/COSvis_DJ/COSvis/App" 
-  ctrl$pathtoCosero <- "D:/Arbeit/2015/003_Projekte/COSvis/in" 
-  ctrl$pathtoApp <- "D:/Arbeit/2015/003_Projekte/COSvis/App"
-#  ctrl$pathtoCosero <- "/Users/ido87/Dropbox/Arbeit-Anderes/scripts_evalCOSwithR" 
-#  ctrl$pathtoApp <- "/Users/ido87/Documents/COSvis/App" 
+ ctrl$pathtoCosero  <- file.choose() %>% strsplit("/") %>% .[[1]]%>% .[1:(length(.)-1)] %>% paste(.,collapse = "/")
+ ctrl$pathtoApp <- file.choose() %>% strsplit("/") %>% .[[1]]%>% .[1:(length(.)-1)] %>% paste(.,collapse = "/")
 # folder names:
-  ctrl$ofoldername <- "output_60min_finalValidierung"
+  ctrl$ofoldername <- "test"
 # Interactive Overview: 
   ctrl$ctrl_span  	<- c(2009,2012) 
 # OF plot options:
