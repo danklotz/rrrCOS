c88eee6619bf7112e7822f887eda93fc497277a0
Daniel Klotz <daniel.klotz@boku.ac.at>
2016-01-21 15:24:19 +0100

    tested fetch-function, should be fine

--- R/fetch.R
+++ R/fetch.R
@@ -2,17 +2,25 @@
 #' 
 #' @export
 #' @examples 
-#' runoff_data <- fetch(what = "runoff_example)
-#' number_of_basins <- fetch(runoff_data, "number_of_basins")
+#' runoff_data <- fetch(runoff_example) 
+#' head(runoff_data)
+#' fetch(number_of_basins,channel.removeChunk(runoff_data))
 fetch <- function(what, ...) {
   # def
-  #assert.Chunk(runoff_data)
-  #assert.dataframe(runoff_data)
-  
+  input <- substitute(what)
+  if ( is.character(input) ) {
+    choice <- input 
+  } else if (is.name(input)) {
+    choice <- deparse(input)
+  } else {
+    stop( paste("Cannot fetch. The option >>", what[1],"<< is neither a name nor a character!", sep = " ") )
+  }
   # 
-  switch(what, 
+  switch(choice, 
          number_of_basins = fetch.number_of_basins(...),
          runoff_example = fetch.runoff_example(),
-         stop( paste("The option >>",what,"<< does not exist as a selection for fetch", sep = " " ) )
+         ctrl = fetch.ctrl(), 
+         hydyears = fetch.hydyears(...),
+         stop( paste("The option >>",what[1],"<< does not exist as a selection for fetch", sep = " " ) )
          )
 }
