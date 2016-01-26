08c5f737eed3e772683f1c5472ce051d7c4b40c2
Herrnegger <mathew.herrnegger@boku.ac.at>
2016-01-26 13:19:06 +0100

    More general handling of noData values in QObs

--- R/channel_only-Observed.R
+++ R/channel_only-Observed.R
@@ -14,12 +14,12 @@ channel.only_observed <- function(runoff_data) {
   # calc
   runoff_data[is.na(runoff_data)] <- -999 
   colmax <- lapply(X = runoff_data, FUN = max) # get max of any column
-  if ( any(colmax == -999) ){
+  if ( any(colmax < 0.0) ){
     # make shure that there are only qobs which have max -999
-    idx_temp <- which(colmax == -999) 
+    idx_temp <- which(colmax < 0.0) 
     OnlyQobsSelected <- names(idx_temp) %>% tolower %>% grepl("qobs.*",.) %>% any
     if (!OnlyQobsSelected){
-      stop("There are Qsim withouth simulation (i.e. only -999 values). Pls remove them first")
+      stop("There are Qsim withouth simulation (i.e. only values smaller 0). Pls remove them first")
     }
     idx_slct <- c(idx_temp,idx_temp + 1) %>% sort() # we add +1 to the idx to get also the simulations
     d_onlyObserved <- runoff_data[-idx_slct]
