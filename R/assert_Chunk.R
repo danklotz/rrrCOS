c0ac2d46a0834df4e6fa6b5a3e1913ff869dc4d6
Daniel Klotz <daniel.klotz@boku.ac.at>
2016-03-10 23:27:23 +0100

    Update assert_Chunk.R

--- R/assert_Chunk.R
+++ R/assert_Chunk.R
@@ -1,9 +1,9 @@
 # test for chunkin data
 #
-# tests if there is still chunk in the runoff_data data frame. 
+# Tests if there is still chunk in the runoff_data data frame. 
 # Where chunk is every data frame except does definded by `fetch.regex_for_runoff_data`
 assert.chunk <- function(runoff_data) {
-  require(magrittr)
+  require(magrittr, quietly = TRUE)
   regEx <- fetch.regex_for_runoff_data()
   assertChunk <- names(runoff_data) %>% tolower %>% grepl(regEx,.)
   if (any(assertChunk == FALSE)) stop("there is still chunk in the data, try: channel.removeChunk")
