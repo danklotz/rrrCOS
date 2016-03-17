9b02b8873999559216fb04c570f6f4f867a28a38
Daniel Klotz <daniel.klotz@boku.ac.at>
2016-03-17 14:12:31 +0100

    making the shiny app

--- R/serve_ofun.R
+++ R/serve_ofun.R
@@ -4,20 +4,24 @@
 #' @return data.frame contianing basic OF
 #' @export
 serve_ofun <- function(obs,sim) {
-  require(hydroGOF)
-  require(magrittr)
+  require("hydroGOF", quietly = TRUE)
+  require("magrittr", quietly = TRUE)
   # calc
   out <- data.frame(
     RMSE = -999,
-    corr = -999,
+    pbias = -999,
     NSE = -999,
     KGE = -999,
-    pbias = -999
+    corr = -999,
+    beta =  -999,
+    alpha =  -999
   )
   out$RMSE <- rmse(sim,obs) %>% as.numeric
-  out$corr <- cor(sim,obs) %>% diag(.)
   out$NSE <- NSE(sim,obs) %>% as.numeric
-  out$KGE <- KGE(sim,obs) %>% as.numeric
   out$pbias <- pbias(sim,obs)
+  out$KGE <- KGE(sim,obs) %>% as.numeric
+  out$corr <- cor(obs,sim) 
+  out$beta <- tmp[1] %>% as.numeric
+  out$alpha <- tmp["KGE.elements.r"] %>% as.numeric
   return(out)
 }
