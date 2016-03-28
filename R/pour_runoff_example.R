94711018e6c4307c77953f7aca1b344bc750d469
Daniel Klotz <daniel.klotz@boku.ac.at>
2016-03-28 23:28:38 +0200

    Update pour_runoff_example.R

--- R/pour_runoff_example.R
+++ R/pour_runoff_example.R
@@ -1,6 +1,6 @@
 #' Get an example runoff_data
 #'
-#'
+#' Gives a prototypcial example of runoff data
 #' @export
 pour_runoff_example <- function() {
 example <- structure(list(yyyy = c(2009L, 2009L, 2009L, 2009L, 2009L, 2009L,
