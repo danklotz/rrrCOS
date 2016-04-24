1cc452928691dbf2a9f904d672f4c692c2d63252
Daniel Klotz <ido87@Daniels-iMac.local>
2016-04-25 01:55:50 +0200

    added idea for o-fun visualisation

--- r_to_add/OF_visualisation_test.R
+++ r_to_add/OF_visualisation_test.R
@@ -5,23 +5,59 @@ require(dplyr, quietly = TRUE)
 require(ggplot2, quietly = TRUE)
 require(shiny, quietly = TRUE)
 #
-a <- seq(-20,20)
+a <- seq(-50,50)/10
 test <- expand.grid(p1 = a, p2 = a) %>% as.data.frame()
-test$p3 <- 0.5
+test$p3 <- 0
 test$p4 <- 1
 
-z_fun <- function(data_frame) {
+z_fun <- function(data) {
   require(dplyr, quietly = TRUE)
-  g <- data_frame
-  z <- ( (1-g$p1^2) + (1-g$p2^2)*sin( (1:length(g$p2))*g$p3 ) ) * test$p4 
+  g <- data
+  z <-  (1-g$p1^2)+5*sin( (1:length(g$p1))*g$p3 ) + (1-g$p2^2)+6*cos( (1:length(g$p2))*g$p4 )
   return( cbind(g,z) )
 } 
-g <- z_fun(test)
 
 plot_fun <- function(data_frame) {
   the_plot <- ggplot(data_frame, aes(x = p1, y = p2, z = z)) +
-    geom_raster(aes(fill = z), interpolate = TRUE) + scale_fill_gradient(low="green", high="red") + 
+    geom_raster(aes(fill = z), interpolate = TRUE) + scale_fill_gradient(low="palevioletred", high="green") + 
     stat_contour()
   return(the_plot)
 }
+
+test_gadget <- function(data) {
+  ui <- miniPage(
+    gadgetTitleBar("Test ofun"),
+    miniContentPanel(
+      # The brush="brush" argument means we can listen for
+      # brush events on the plot using input$brush.
+      fillCol( flex = c(3, 1),
+        plotOutput("plot", height = "100%"),
+        fillRow(
+          sliderInput("in_p3", "p3", -1, 1, 0, step = 0.01, sep = "", animate = TRUE),
+          sliderInput("in_p4", "p4", -1, 1, 0, step = 0.01, sep = "")
+        )
+      )
+    )
+  )
+  server <- function(input, output, session) {
+    updated_data <- reactive({ data.frame(p1 = data$p1,
+                                          p2 = data$p2, 
+                                          p3 = as.numeric(input$in_p3),
+                                          p4 = as.numeric(input$in_p4))})
+    output$plot <- renderPlot({
+      z_data <-  z_fun(updated_data())
+      plot_fun(z_data)
+    })
+    # Handle the Done button being pressed.
+    observeEvent(input$done, {
+      # Return the brushed points. See ?shiny::brushedPoints.
+      stopApp("yea, yea, yea")
+    })   
+  }
+  #
+  runGadget(ui, server)
+}  
+test_gadget(test)
+  
+
 
