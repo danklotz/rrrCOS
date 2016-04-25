75a269ca71e5f5c8e9bfaba5edbe7aee7015556a
Daniel Klotz <daniel.klotz@boku.ac.at>
2016-04-25 17:44:41 +0200

    implemented VARS -multivariate gaussian functionality + some parameters

--- r_to_add/OF_visualisation_test.R
+++ r_to_add/OF_visualisation_test.R
@@ -4,60 +4,74 @@ require(magrittr, quietly = TRUE)
 require(dplyr, quietly = TRUE)
 require(ggplot2, quietly = TRUE)
 require(shiny, quietly = TRUE)
+require(miniUI, quietly = TRUE)
+require(VARS, quietly = TRUE)
 #
-a <- seq(-50,50)/10
-test <- expand.grid(p1 = a, p2 = a) %>% as.data.frame()
-test$p3 <- 0
-test$p4 <- 1
 
-z_fun <- function(data) {
+z_fun <- function(grid_points, mean, sigma, rho) {
   require(dplyr, quietly = TRUE)
-  g <- data
-  z <-  (1-g$p1^2)+5*sin( (1:length(g$p1))*g$p3 ) + (1-g$p2^2)+6*cos( (1:length(g$p2))*g$p4 )
-  return( cbind(g,z) )
+  z <- multi_norm(grid_points,mean,sigma,rho)
+  return( data.frame(grid_points,z))
 } 
 
 plot_fun <- function(data_frame) {
-  the_plot <- ggplot(data_frame, aes(x = p1, y = p2, z = z)) +
+  the_plot <- ggplot(data_frame, aes(x = par1, y = par2, z = z)) +
     geom_raster(aes(fill = z), interpolate = TRUE) + scale_fill_gradient(low="palevioletred", high="green") + 
-    stat_contour()
+    stat_contour(color = "white")
   return(the_plot)
 }
 
-test_gadget <- function(data) {
+test_gadget <- function(n, sigma, rho) {
   ui <- miniPage(
     gadgetTitleBar("Test ofun"),
     miniContentPanel(
       # The brush="brush" argument means we can listen for
       # brush events on the plot using input$brush.
-      fillCol( flex = c(3, 1),
+      fillCol( flex = c(4, 1),
         plotOutput("plot", height = "100%"),
         fillRow(
-          sliderInput("in_p3", "p3", -1, 1, 0, step = 0.01, sep = "", animate = TRUE),
-          sliderInput("in_p4", "p4", -1, 1, 0, step = 0.01, sep = "")
+          sliderInput("z_axis", "z:", -2, 2, 0, step = 0.01, sep = "", animate = TRUE),
+          sliderInput("mean1", "mean1:", -1, 1, 0.1, step = 0.01, sep = ""),
+          sliderInput("mean2", "mean2:", -1, 1, 0.1, step = 0.01, sep = ""),
+          sliderInput("mean3", "mean3:", -1, 1, 0.1, step = 0.01, sep = "")
         )
       )
     )
   )
   server <- function(input, output, session) {
-    updated_data <- reactive({ data.frame(p1 = data$p1,
-                                          p2 = data$p2, 
-                                          p3 = as.numeric(input$in_p3),
-                                          p4 = as.numeric(input$in_p4))})
-    z_data <-  reactive({  z_fun(updated_data()) })
-    output$plot <- renderPlot({
-      plot_fun(z_data())
+    grid_points <- reactive({
+      expand.grid(par1 = seq(-1, 1, length.out = n),
+                  par2 = seq(-1, 1, length.out = n),
+                  x3 = as.numeric(input$z_axis))
     })
+    set_mean <- reactive({
+      c( as.numeric(input$mean1),as.numeric(input$mean2), as.numeric(input$mean3) )
+    })
+    z_data <-  reactive({
+      z_fun( grid_points(), mean = set_mean(), sigma, rho )  
+    })
+    output$plot <- renderPlot({
+      plot_fun( z_data() )
+      })
     # Handle the Done button being pressed.
     observeEvent(input$done, {
       # Return the brushed points. See ?shiny::brushedPoints.
-      stopApp(max(z_data()$z))
+      stopApp("yeah, yeah, yeah")
     })   
   }
   #
   runGadget(ui, server)
 }  
-test_gadget(test)
+
+
+
+
+# a <- seq(-1, 1, length.out = 30)
+# grid_points <- expand.grid(x1 = a, x2 = a, x3 = a)
+mean <- c(0.2,0.5,0)
+sigma <- c(0.7,1,0.5)
+rho <- c(0,0.3,0.9)
+test_gadget(30, sigma, rho)
 
   
 
--- r_to_add/kge_sensitivity.R
+++ r_to_add/kge_sensitivity.R
@@ -1,23 +1,62 @@
 test_KGE <- function(obs,sim) {
   # def:
     require(hydroGOF,quietly = TRUE)
-    
+    require(dplyr, quietly = TRUE)
   # calc:
   length_loop <- length(obs)
-  ref_kge <- hydroGOF::KGE(sim,obs)
-  change_KGE <- seq(1,length_loop)
-  test <- sapply(change_KGE, sub_fun, obs = obs, sim = sim, ref_kge =ref_kge) 
-  
-} 
- 
- 
-  sub_fun <- function(x,obs,sim,ref_kge) {
+  data <- data.frame(idx = 1:length_loop,obs = obs, sim = sim)
+  sub_fun <- function(data, x) {
     require(hydroGOF,quietly = TRUE)
-    x_obs <- c(obs[1:x-1],obs[x+1:length_loop])
-    x_sim <- c(sim[1:x-1],sim[x+1:length_loop])
-    x_kge <- hydroGOF::KGE(x_sim,x_obs)
+    require(dplyr, quietly = TRUE)
+    #
+    mod_data <- filter(data, idx != x)
+    x_kge <- hydroGOF::KGE(mod_data$sim,mod_data$obs)
     return(x_kge)
   }
+  change_KGE <- seq(1,length_loop)
+  test <- sapply(change_KGE, sub_fun, data = data) 
   
+} 
+#
+require("data.table")
+require("magrittr", quietly = TRUE)
+pathDotRunoff  <- file.choose()
+d_raw <- fread(pathDotRunoff, check.names = TRUE, header = TRUE, skip = 22) %>%
+  as.data.frame(.)
+names(d_raw)[5] <- "min"
+#
+obs <- d_raw$QOBS_0003
+sim <- d_raw$QSIM_0003
+abs_deviation <- test_KGE(obs,sim)
+rel_deviation <- abs_deviation/hydroGOF::NSE(sim,obs)
+# plot:
+plot(obs,type = "l", col = "blue"); 
+par(new = T); 
+plot(sim, type = "l", col = "red",  axes= FALSE, xlab= NA, ylab=NA);
+par(new = T)
+plot(rel_deviation, type = "l", col = "magenta",  axes= FALSE, xlab=NA, ylab=NA); 
+axis(side = 4);
+mtext(side = 4, line = 3, 'relative change of KGE')
+  
+
+
+# test_KGE <- function(obs,sim) {
+#   # def:
+#   require(hydroGOF,quietly = TRUE)
+#   
+#   # calc:
+#   length_loop <- length(obs)
+#   sub_fun <- function(x,obs,sim,ref_kge) {
+#     require(hydroGOF,quietly = TRUE)
+#     x_obs <- c(obs[1:(x-1)],obs[(x+1):length_loop])
+#     x_sim <- c(sim[1:(x-1)],sim[(x+1):length_loop])
+#     x_kge <- hydroGOF::KGE(x_sim,x_obs)
+#     return(x_kge)
+#   }
+#   ref_kge <- hydroGOF::KGE(sim,obs)
+#   change_KGE <- seq(1,length_loop)
+#   test <- sapply(change_KGE, sub_fun, obs = obs, sim = sim, ref_kge =ref_kge) 
+#   
+# } 
   
   
