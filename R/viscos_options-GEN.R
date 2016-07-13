2b27c34fc539d27fd266af4ee25c01663748ca9c
JWBoku <johannes.wesemann@boku.ac.at>
2016-07-13 15:53:46 +0200

    Options_Example added; first cooking adaptions

--- R/viscos_options-GEN.R
+++ R/viscos_options-GEN.R
@@ -6,31 +6,37 @@
 #' (default values)
 #' \preformatted{
 #'   viscos_options(
-#'     name_data1 = "qobs",
-#'     name_data2 = "qsim"
-#'     name_COSyear = "yyyy",
-#'     name_COSmonth = "mm",
-#'     name_COSday = "dd",
-#'     name_COShour = "hh",
-#'     name_COSmin = "min",
-#'     name_COSposix = "posixdate",
-#'     name_COSperiod = "period",
-#'     # plot stuff
-#'     color_data1 = "steelblue", 
-#'     color_data2 = "palevioletred",
-#'     plot_title = "title",
-#'     legend_title = "legend",
-#'     xlab = "year",
-#'     clr1 = '#FF3300',
-#'     clr2 = '#f6f3a1',
-#'     clr3 = '#005900',
-#'     clr4 = "purple4",
-#'     midpoint = 0.5,
-#'     limits = c(0,1),
-#'     lb_cut = 0.0,
-#'     text_size = 0.5)}
+#'    # data.frame column names
+#'       name_data1 = "qobs",
+#'       name_data2 = "qsim"
+#'       name_COSyear = "yyyy",
+#'       name_COSmonth = "mm",
+#'       name_COSday = "dd",
+#'       name_COShour = "hh",
+#'       name_COSmin = "min",
+#'       name_COSposix = "posixdate",
+#'       name_COSperiod = "period",
+#'    # plot options
+#'       color_data1 = "steelblue", 
+#'       color_data2 = "palevioletred",
+#'       plot_title = "title",
+#'       legend_title = "legend",
+#'       xlab = "year",
+#'       clr1 = '#FF3300',
+#'       clr2 = '#f6f3a1',
+#'       clr3 = '#005900',
+#'       clr4 = "purple4",
+#'       midpoint = 0.5,
+#'       limits = c(0,1),
+#'       lb_cut = 0.0,
+#'       text_size = 0.5)}
 #'
+#' @examples
+#' viscos_options("name_data1")
+#' viscos_options(name_data1 = "OtherData")
+#' viscos_options("name_data1")
 #' @export
+
 
 
 
