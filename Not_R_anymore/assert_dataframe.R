3ec4799e3ffb172d9d1d686150087c0f83f2b788
Daniel Klotz <daniel.klotz@boku.ac.at>
2016-07-13 15:32:49 +0200

    more stuff deleted. Maybe to much. But I htink it will be fine

--- Not_R_anymore/assert_dataframe.R
+++
@@ -1,6 +0,0 @@
-# test if data is a data frame
-# 
-# returns error message if the input: "data" is not of class "data.frame" 
-assert_dataframe <- function(data) {
-  if ( !is.data.frame(data) ) stop("runoff_data is no data_frame!")
-}
--- Not_R_anymore/serve.plotlist_periodOF.R
+++ Not_R_anymore/serve.plotlist_periodOF.R
@@ -4,14 +4,12 @@
 #' creates a list of bar-plots for a the yearly objective functions (OF) xxx
 #' see: description-follows-soonTM xxx
 #' @export
-serve.plotlist_periodOF <- function(runoff_data,plt_ctrl,bOF) {
-  # ofun_hydyearly,periods,num_basins,plt_ctrl) {
+serve.plotlist_periodOF <- function(runoff_data,bOF) {
     require("ggplot2", quietly = TRUE)
-    if (missing(bOF)) {bOF <- extract_of(runoff_data)}
+    if (missing(bOF)) {bOF <- extract_objective_functions(runoff_data)}
   # calc: -------------------------------------------------------------------
-  ofun_hydyearly <- bOF$NSE_periods
-  periods <- unique(runoff_data$period) %>% extract(.>0)
-  num_basins <- get_basin_numbers(d_runoff)
+  ofun_hydyearly <- bOF$NSE_period
+  num_basins <- get_basin_numbers(runoff_data)
   # 
   rename.withEndings <- function(dataframe,name,endings) {
     new_names <- paste(name,endings,sep = "")
@@ -19,34 +17,34 @@ serve.plotlist_periodOF <- function(runoff_data,plt_ctrl,bOF) {
     return(dataframe)
   }
   d_ofun_yearly <- ofun_hydyearly %>% 
-                    pmax(.,plt_ctrl$lb_cut) %>%
+                    pmax(.,viscos_options()$lb_cut) %>%
                     as.data.frame %>%
-                    rename.withEndings(.,plt_ctrl$plot_title,num_basins)
+                    rename.withEndings(.,viscos_options()$plot_title,num_basins)
   new_names <- names(d_ofun_yearly)
-  d_ofun_yearly$hydyear <- periods
+  d_ofun_yearly$hydyear <- unique(runoff_data$period) %>% extract(.>0)
   # make list of plots
   list_of_barplots <- list()
   eval_size <- ncol(ofun_hydyearly)
   # define plot function: 
   plot_function <- function(k) {
     ggplot(data = d_ofun_yearly,environmnet = environment()) +
-    geom_bar(stat="identity",
+    geom_bar(stat = "identity",
              position = "identity",
-             aes_string(x= "hydyear", y = new_names[k],fill = new_names[k])) +
+             aes_string(x = "hydyear", y = new_names[k],fill = new_names[k])) +
     theme_bw(base_size = 15) +
     ggtitle(new_names[k]) +
-    xlab(plt_ctrl$xlab) +
-    ylab(plt_ctrl$ylab ) +
-    scale_y_continuous(limits = plt_ctrl$limits ) +
-    theme(legend.position= "none",
+    xlab(viscos_options()$xlab) +
+    ylab( "NSE" ) +
+    scale_y_continuous(limits = viscos_options()$limits ) +
+    theme(legend.position = "none",
           axis.text.x = element_text(angle = 50, hjust = 1),
           plot.margin = grid::unit(c(0.2,0.5,0.2,0.5), "cm") ) + # clockwise form above
     scale_fill_gradient2(space = "Lab",
-                         low = plt_ctrl$clr1,
-                         mid = plt_ctrl$clr2,
-                         high = plt_ctrl$clr3,
-                         midpoint = plt_ctrl$midpoint,
-                         limits = plt_ctrl$limits )
+                         low = viscos_options()$clr1,
+                         mid = viscos_options()$clr2,
+                         high = viscos_options()$clr3,
+                         midpoint = viscos_options()$midpoint,
+                         limits = viscos_options()$limits )
   }
   # apply plot function over all available stuff
   list_of_barplots <- lapply(1:eval_size,
