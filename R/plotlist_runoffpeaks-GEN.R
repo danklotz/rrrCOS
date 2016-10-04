85d0f72a162a735d0e4d8ea8370f93bc4a0f3987
Daniel Klotz <daniel.klotz@boku.ac.at>
2016-10-04 15:17:05 +0200

    better get runoff_peaks
    event peaks now have a name + 
    some defensive code

--- R/plotlist_runoffpeaks-GEN.R
+++ R/plotlist_runoffpeaks-GEN.R
@@ -1,38 +1,57 @@
 #' Plot List for Runoff Peaks 
 #' @export
+#' 
+#' @import ggplot2 
+#' @import dplyr 
+#' @import magrittr
+#' @importFrom tibble tibble
 plotlist_runoffpeaks <- function(runoff_data,
-                                 n_events = 10,
-                                 window_size = 24) {
-  require("ggplot2", quietly = TRUE)
-  require("magrittr", quietly = TRUE)
-  #
+                                 n_events= 10L,
+                                 window_size = 24L) {
+  # pre:
+  assert_dataframe(runoff_data)
+  n_events_int <- as.integer(n_events) 
+  window_size_int <- as.integer(window_size)
+  if( is.na(n_events_int)  | 
+      is.nan(n_events_int) | 
+      is.infinite(n_events_int) | 
+      !is.integer(n_events_int) ) {
+    stop("n_events is ill defined")
+  }
+  if( is.na(window_size)  | 
+      is.nan(window_size) | 
+      is.infinite(window_size) | 
+      !is.integer(window_size) ) {
+    stop("window_size is ill defined")
+  }
   data1 <- runoff_data %>%
-    dplyr::select(.,dplyr::starts_with(viscos_options("name_data1")))
+     select( starts_with(viscos_options("name_data1")) )
   data2 <- runoff_data %>%
-    dplyr::select(.,dplyr::starts_with(viscos_options("name_data2")))
+     select( starts_with(viscos_options("name_data2")) )
   data_numbers <- names(data1) %>%
     gsub(viscos_options("name_data1"),"",.,ignore.case = TRUE) %>% 
     gsub("\\D","",.,ignore.case = TRUE)
+  # make plotlist: 
   plotlist <- lapply(1:ncol(data1), function(x) plotlist_one_basin(data1[,x],
                                                                    data2[,x],
-                                                                   n_events, 
-                                                                   window_size)) %>%
-    rename_peakslist(.,data_numbers)
+                                                                   n_events_int, 
+                                                                   window_size_int)) %>%
+    set_names(.,paste("basin", data_numbers, sep =""))
   return(plotlist)
 }
-plotlist_one_basin <- function(qobs,qsim,n_events,window_size) {
+plotlist_one_basin <- function(qobs,qsim,n_events_int,window_size_int) {
    #### calc:
   single_data <- tibble::tibble(time = as.integer(1:length(qobs)),
                                 obs = as.double(qobs),
                                 sim = as.double(qsim))
   #
-  peak_idx <- find_peaks(single_data$obs,m = window_size)
+  peak_idx <- find_peaks(single_data$obs,m = window_size_int)
   peak_organised <- tibble::tibble(idx = as.integer(peak_idx), 
                            peak_obs = single_data$obs[peak_idx], 
                            peak_sim = single_data$sim[peak_idx])
   highest_peaks_organised <- peak_organised$peak_obs %>% 
     sort(decreasing = TRUE) %>% 
-    .[1:n_events] %>%
+    .[1:n_events_int] %>%
     '%in%'(peak_organised$peak_obs,.) %>% 
     which( . ) %>% 
     peak_organised[., ]
@@ -44,21 +63,14 @@ plotlist_one_basin <- function(qobs,qsim,n_events,window_size) {
   overview_scatter <- ggplot(highest_peaks_organised) + 
     geom_abline(color = viscos_options("color_of_mid")) +
     geom_point(aes(peak_obs,peak_sim), size = 4) +
-    expand_limits(x = 0, y = 0)
+    expand_limits(x = 0, y = 0) 
   sub_plots <- lapply(1:nrow(highest_peaks_organised),
-                      function(x) sub_peakplot_fun(x,window_size,highest_peaks_organised,single_data) )
+                      function(x) sub_peakplot_fun(x,window_size_int,highest_peaks_organised,single_data) ) %>%
+    set_names(.,paste("event_plot",1:length(.),sep=""))
   return(overview = append(list(overview = overview_plot,scatter = overview_scatter), sub_plots))
 }
-# helper function to give the list some names
-rename_peakslist <- function(data_list,data_numbers) {
-  d_length <- length(data_list)
-  d_names <- paste("basin", data_numbers, sep ="")
-  names(data_list) <- d_names
-  return(data_list)
-}
   ####
-  # peak finder function 
-    #   propsoed by http://stats.stackexchange.com/questions/22974/how-to-find-local-peaks-valleys-in-a-series-of-data
+  #   propsoed by http://stats.stackexchange.com/questions/22974/how-to-find-local-peaks-valleys-in-a-series-of-data
   #   btw. also a nice idea: http://stats.stackexchange.com/questions/36309/how-do-i-find-peaks-in-a-dataset
   find_peaks <- function (x, m = 3){
     shape <- diff(sign(diff(x, na.pad = FALSE)))
