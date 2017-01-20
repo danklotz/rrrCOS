b824a9caa43d0e7e204d196295d24a244b948909
Daniel Klotz <daniel.klotz@boku.ac.at>
2017-01-20 19:43:47 +0100

    created new bookdown documents

--- Render_AllRmd.R
+++ Render_AllRmd.R
@@ -1,7 +1,17 @@
-# Render all html files
-require(rmarkdown)
-setwd("vignettes")
-Rmd_files <- list.files(".",pattern = "*.Rmd")
-for (name in Rmd_files){
-  render(name)
-}
+# Render html files -------------------------------------------------------
+
+# old method ==============================================================
+# require(rmarkdown)
+# setwd("vignettes")
+# Rmd_files <- list.files(".",pattern = "*.Rmd")
+# for (name in Rmd_files){
+#   render(name)
+# }
+
+# new method ==============================================================
+  require(bookdown)
+  setwd("vignettes")
+  # generate pdf file 
+  render_book("index.Rmd", "bookdown::pdf_book")
+  # generate html book
+  render_book("index.Rmd", "bookdown::gitbook")
