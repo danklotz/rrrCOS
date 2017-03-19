a2ea82a8035c9e51e4f68c8fe2e3dd6701a4ab0e
Daniel Klotz <daniel.klotz@boku.ac.at>
2017-03-19 15:51:56 +0100

    started to implement of_window function
    lot of defenses are missing, otherwise it looks good

--- R/time_aggregation-GEN.R
+++ R/time_aggregation-GEN.R
@@ -21,6 +21,8 @@
     if (grepl("mm",aggregation)) {
       cutting_bounds[1] <- min(6,cutting_bounds[1])
       cutting_bounds[2] <- max(7,cutting_bounds[2])
+      interval_bounds <- c(min = cos_data[viscos_options("name_COSmonth")] %>% min(.),
+                           max = cos_data[viscos_options("name_COSmonth")] %>% max(.))
     }
     if (grepl("yyyy",aggregation)) {
       cutting_bounds[1] <- min(1,cutting_bounds[1])
@@ -30,8 +32,13 @@
     regex_for_cos_selection <- viscos_options("name_o") %|%  viscos_options("name_s")
     # aggregation function:
     aggregator_fun <- function(k,data_frame){
-      the_aggregation <- aggregate(data_frame[[k]] ~ data_frame$date_selection, FUN = mean)
-      return(the_aggregation[ ,2])
+      interval <- tibble(idx = seq(interval_bounds["min"],interval_bounds["max"]), 
+                         value = NA)
+      temp_aggregation <- stats::aggregate(data_frame[[k]] ~ data_frame$date_selection, FUN = mean, simplify = FALSE)
+      interval$value[
+        interval$idx %in% 
+          as.integer(temp_aggregation$`data_frame$date_selection`) ] <- unlist(temp_aggregation$`data_frame[[k]]`)
+      return(interval$value)
     }
     #####
     # If cos_data is not provided fully, the date is completed automatically
